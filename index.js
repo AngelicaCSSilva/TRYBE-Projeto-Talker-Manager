c86a858b3c220aedde4c87af2c662f5d249d6c8b
Angélica Silva <angelica.silva9323@gmail.com>
2022-05-17 18:14:42 -0300

    feat: :sparkles: Add function to read talkers.json

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Função para leitura do arquivo talker.json
+const readTalkersFile = async () => {
+  const rawFile = await fs.readFile('./talker.json', 'utf8');
+  return JSON.parse(rawFile);
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
