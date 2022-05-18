91d5f6f14b91dc2483c03b12ef99cf75eed1fbbd
Angélica Silva <angelica.silva9323@gmail.com>
2022-05-18 17:24:06 -0300

    feat: :construction: Added middleware to validate talk

--- middleware/talkervalidation.js
+++ middleware/talkervalidation.js
@@ -29,8 +29,21 @@ const ageValidation = (req, res, next) => {
   next();
 };
 
+const talkValidation = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+  });
+  }
+
+  next();
+};
+
 module.exports = {
   tokenValidation,
   nameValidation,
   ageValidation,
+  talkValidation,
 };
