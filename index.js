73f70cf18c7545cb9b53dd784a82c0a4a0d86cee
Angélica Silva <angelica.silva9323@gmail.com>
2022-05-18 13:01:43 -0300

    feat: :sparkles: Added endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -29,3 +29,15 @@ app.get('/talker', async (_req, res) => {
   const talkersFile = await readTalkersFile();
   return res.status(HTTP_OK_STATUS).json(talkersFile);
 });
+
+// Endpoint GET /talker/:id
+/* A requisição retorna o status 200 e uma pessoa palestrante com base no id da rota. Caso não haja ninguém com esse id, deve retornar o status 404 com a mensagem 'Pessoa palestrante não encontrada'. */
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkersFile = await readTalkersFile();
+  const foundSpeaker = talkersFile.find((speaker) => speaker.id === +id);
+  if (foundSpeaker) {
+    return res.status(HTTP_OK_STATUS).json(foundSpeaker);
+  }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
