583510fe1a4b71bb8692e2780bfa0558d74105b0
Angélica Silva <angelica.silva9323@gmail.com>
2022-05-18 17:23:20 -0300

    feat: :construction: Added middleware to validate name

--- middleware/talkervalidation.js
+++ middleware/talkervalidation.js
@@ -7,7 +7,18 @@ const tokenValidation = (req, res, next) => {
   next();
 };
 
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+  
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
+  }
+
+  next();
+};
 
 module.exports = {
   tokenValidation,
+  nameValidation,
 };
