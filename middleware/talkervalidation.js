6c39731f1d907d1c60cefa83c25a8f4429acd4ae
Angélica Silva <angelica.silva9323@gmail.com>
2022-05-18 17:23:43 -0300

    feat: :construction: Added middleware to validate age

--- middleware/talkervalidation.js
+++ middleware/talkervalidation.js
@@ -18,7 +18,19 @@ const nameValidation = (req, res, next) => {
   next();
 };
 
+const ageValidation = (req, res, next) => {
+  const { age } = req.body;
+  
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (+age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' }); 
+  }
+
+  next();
+};
+
 module.exports = {
   tokenValidation,
   nameValidation,
+  ageValidation,
 };
